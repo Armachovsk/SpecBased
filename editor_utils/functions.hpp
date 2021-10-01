bbd75b022e6c506b86dbbbd6ebab19d8b68f8b05
Reidond <reidond@gmail.com>
2021-10-02 01:02:38 +0300

    feat: add rest of functions and redefines

---
+++ editor_utils/functions.hpp
@@ -0,0 +1,10 @@
+// Just to have backwards compat
+class Gemini {
+    class main {
+        file = "specbased\editor_utils\fnc";
+
+        class lightSource {};
+
+        class playAnimation {};
+    }
+}
